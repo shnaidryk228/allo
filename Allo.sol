8c23a9c5db9ded44c2e63b9f38f91d37867f3f53
shnaidryk228 <117035749+shnaidryk228@users.noreply.github.com>
2023-10-16 16:17:07 +0300

    Update Allo.sol

--- Allo.sol
+++ Allo.sol
@@ -198,3 +198,7 @@ baseFee = _baseFee;
     /// @param _amount The amount of the token
  /// @param _metadata The metadata of the pool
     /// @param _managers The managers of the pool
+ function createPoolWithCustomStrategy(
+        bytes32 _identityId,
+        address _strategy,
+        bytes memory _initStrategyData,
