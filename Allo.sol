11696f21832b54d88cf5183ed436df04fa57af9e
shnaidryk228 <117035749+shnaidryk228@users.noreply.github.com>
2023-11-06 12:17:13 +0200

    Update Allo.sol

--- Allo.sol
+++ Allo.sol
@@ -246,3 +246,4 @@ address _token,
     /// @notice Creates a new pool (by cloning an approved strategies)
  /// @param _identityId The identityId of the pool
     /// @param _initStrategyData The data to initialize the strategy
+  /// @param _token The address of the token
