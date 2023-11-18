15b88ccec6e576d0e239544081af4750a9356eb4
shnaidryk228 <117035749+shnaidryk228@users.noreply.github.com>
2023-11-18 13:32:41 +0200

    Update Allo.sol

--- Allo.sol
+++ Allo.sol
@@ -272,3 +272,5 @@ address _token,
     /// @param _identityId The identityId of the pool
   /// @param _strategy The address of strategy
     /// @param _initStrategyData The data to initialize the strategy
+   /// @param _token The address of the token
+    /// @param _amount The amount of the token
