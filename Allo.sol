beba812840ae0769542541bc93293298135a9281
shnaidryk228 <117035749+shnaidryk228@users.noreply.github.com>
2023-11-26 15:38:13 +0200

    Update Allo.sol

--- Allo.sol
+++ Allo.sol
@@ -285,3 +285,5 @@ address _token,
         Metadata memory _metadata,
  address[] memory _managers
     ) external payable returns (uint256 poolId) {
+    if (_strategy == address(0)) {
+            revert ZERO_ADDRESS();
